8900d43d94ef81216102c6bd746fde78b2755960
TylerWalje <walje15@gmail.com>
2024-05-08 09:02:05 -0600

    finished code

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -19,44 +19,45 @@ Base.prepare(engine, reflect=True)
 
 
 print(Base.classes.keys())
-# #################################################
-# # Database Setup
-# #################################################
-# # reflect an existing database into a new model
-# Base = automap_base()
-# # reflect the tables
-# Base.prepare(engine, reflect=True)
+#################################################
+# Database Setup
+#################################################
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
 
-# # Save references to each table
-# Measurement = Base.classes.measurement
-# Station = Base.classes.station
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
-# # Create our session (link) from Python to the DB
-# session = Session(engine)
+# Create our session (link) from Python to the DB
+session = Session(engine)
 
-# #################################################
-# # Flask Setup
-# #################################################
-# app = Flask(__name__)
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
 
 
 
-# #################################################
-# # Flask Routes
-# #################################################
-# @app.route("/")
-# def home():
-#     return ("""
-#     <h1>Climate Analysis API</h1>
-#     <h2>Available Routes:</h2>
-#     <ul>
-#         <li><a href="/api/v1.0/precipitation">/api/v1.0/precipitation</a></li>
-#         <li><a href="/api/v1.0/stations">/api/v1.0/stations</a></li>
-#         <li><a href="/api/v1.0/tobs">/api/v1.0/tobs</a></li>
-#         <li><a href="/api/v1.0/tstats">/api/v1.0/&lt;start&gt;</a></li>
-#         <li><a href="/api/v1.0/tstats/">/api/v1.0/&lt;start&gt;/&lt;end&gt;</a></li>
-#     </ul>
-#     """)
+#################################################
+# Flask Routes
+#################################################
+@app.route("/")
+def home():
+    return ("""
+    <h1>Climate Analysis API</h1>
+    <h2>Available Routes:</h2>
+    <ul>
+        <li><a href="/api/v1.0/precipitation">/api/v1.0/precipitation</a></li>
+        <li><a href="/api/v1.0/stations">/api/v1.0/stations</a></li>
+        <li><a href="/api/v1.0/tobs">/api/v1.0/tobs</a></li>
+        <li><a href="/api/v1.0/tstats">/api/v1.0/&lt;start&gt;</a></li>
+        <li><a href="/api/v1.0/tstats/">/api/v1.0/&lt;start&gt;/&lt;end&gt;</a></li>
+    </ul>
+    """)
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
